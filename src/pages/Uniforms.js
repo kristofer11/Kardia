7b082f2a0898a8ca2b00d296ffbd4e8ea0dee4b6
Kris Hvattum <klhvattum@live.com>
2023-03-18 16:10:24 -0700

    Add target_blank to school bellese link

--- src/pages/Uniforms.js
+++ src/pages/Uniforms.js
@@ -7,7 +7,7 @@ const Uniforms = () => {
         <h2 className='text-center' style={{fontSize: '3rem'}}>Uniforms</h2>
 
         <p >
-        We will be sticking with the uniform tradition for our in-person days. This year, students will be required to wear formal and daily uniform items. Logo items will be available to order throughout the year. Please see the uniform policy below for more specific information and where to order logo items. Click <a href='http://www.schoolbelles.com/myschool/M37/s2950'>HERE</a> to be relocated to our School Belles uniform page. Our school code is S2950. There are several required items from School Belles.
+        We will be sticking with the uniform tradition for our in-person days. This year, students will be required to wear formal and daily uniform items. Logo items will be available to order throughout the year. Please see the uniform policy below for more specific information and where to order logo items. Click <a href='http://www.schoolbelles.com/myschool/M37/s2950' target='_blank'>HERE</a> to be relocated to our School Belles uniform page. Our school code is S2950. There are several required items from School Belles.
         </p>
         <img style={{maxWidth: '100%', marginBottom: '1rem'}} src={uniforms} className='col-8' alt='document explaining Kardia uniform policy' />
         <p>These are the uniform requirements for the 2023-2024 school year. Please note that there are several changes.</p>
