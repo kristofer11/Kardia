62d13835488b3ebe6df91cf54cd0b91b7733450c
Kris Hvattum <klhvattum@live.com>
2023-03-28 21:24:15 -0700

    Fix paypal link (new donation page on paypal)

--- src/pages/Donations.js
+++ src/pages/Donations.js
@@ -11,7 +11,7 @@ const Donations = () => {
                     Kardia Classical School is a nonprofit private education organization and every bit of money that we raise or is donated goes back to the school and our students. Our goal is to help our program to grow over the next few years. As we grow, we will need a larger space to meet, more quality teachers, materials, and more! We are constantly looking for ways to enhance our program and offer the best to our students and families. Thank you for considering donating to our wonderful little school and our amazing students.
                 </p>
                 <p><em>Click on the PayPal logo below to be routed to our PayPal account.</em></p>
-                <a className='text-center w-100' href='https://www.paypal.com/donate?token=3sb9oYsFuCbGw4QDRm-wFvxiFRvWBPFhhxT8d4AAduCM5BchvSE52sXCwttdI4v_YWAhTX4ebeQRew7e' target='_blank'>
+                <a className='text-center w-100' href='https://www.paypal.com/donate/?hosted_button_id=WAEDRZYKQJJBE' target='_blank'>
                     <img src={payPal} className='col-4 mt-3' />
                 </a>
                 <p className='mt-3'><strong>Your contributions may be tax-deductible</strong>. Kardia Classical School’s Tax ID is #85-1947905. We are an IRS-designated 501 (c)(3) non-profit organization. Please contact your tax advisor for tax deduction advice.</p>
