35a2d0502b6beab94a0041adedec262fc7938605
Kris Hvattum <klhvattum@live.com>
2023-04-23 22:52:25 -0700

    Contact page styling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -21,8 +21,18 @@ const Contact = () => {
                     <h2 className='text-center' style={{color: '#152d7d'}}>We would love to hear from you!</h2>
                     <h4 className='text-center m-4'>You can reach us via phone, email or Facebook Messenger (see the link in the lower right corner).</h4>
 
-                    <p style={{fontSize: '1.8rem'}}>Email: <a className='contact-link' href='mailto: office@kardiaclassical.org'>office@KardiaClassical.org</a></p>
-                    <p  style={{fontSize: '1.8rem'}}> Phone: <a className='contact-link' href='tel: 360-453-7334'>360-453-7334</a></p>
+                    <p className='contact-text' style={{fontSize: '1.8rem'}}>Email: 
+                        <span className='contact-link' id='contact-link'>
+                            <a href='mailto: office@kardiaclassical.org'>office@KardiaClassical.org</a>
+                        </span>
+                    </p>
+
+                    <p  style={{fontSize: '1.8rem'}}> Phone: 
+                        <span className='contact-link'>
+                            <a href='tel: 360-453-7334' >360-453-7334</a>
+                        </span>
+                    </p>
+                    
                 </div>
 
             </div>
