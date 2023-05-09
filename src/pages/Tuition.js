7ac6705db026e161f1bd28d226589cb96b4ac4da
Kris Hvattum <klhvattum@live.com>
2023-05-08 21:59:41 -0700

    Fix typo on tuition page, add PreK tuition, add PreK Uniform

--- src/pages/Tuition.js
+++ src/pages/Tuition.js
@@ -1,5 +1,6 @@
-import tuition from '../assets/enroll/Tuition-2023-2024.png';
+import tuition from '../assets/enroll/Tuition 2023.png';
 import Tuesday from '../assets/Tuesday-Camp-2023-2024-768x113.png';
+import Prek from '../assets/enroll/Pre-K Tuition 2023-2024.png'
 
 const Tuition = () => {
     return (
@@ -9,8 +10,9 @@ const Tuition = () => {
                 <h3 className='text-center pt-4'>
                     Now accepting applications for the 2023-2024 school year!
                 </h3>
+                <img src={Prek} className='col-12' />
                 <img src={tuition} className='col-12 mb-5' />
-                <p><strong>Plesase note, there is a $50 per family application fee for new families.</strong><em> This is a one-time fee.</em></p>
+                <p><strong>Please note, there is a $50 per family application fee for new families.</strong><em> This is a one-time fee.</em></p>
                 <p >
                     Visit our <a href='/Programs'>Program Options</a> page to see all of the options for the 2023-2024 school year.
                 </p>
--- src/pages/Uniforms.js
+++ src/pages/Uniforms.js
@@ -1,18 +1,20 @@
-import uniforms from '../assets/Uniform-Policy-2023-2024-1187x1536.png'
+import uniforms from '../assets/Uniform-Policy-2023-2024-1187x1536.png';
+import prekuniforms from '../assets/Pre-K Uniform Policy.png';
 
 const Uniforms = () => {
-  return (
-    <div className="page-content">
-    <div id='whatIsClassical'  className='info-block text-center'>
-        <h2 className='text-center' style={{fontSize: '3rem'}}>Uniforms</h2>
+    return (
+        <div className="page-content">
+            <div id='whatIsClassical' className='info-block text-center'>
+                <h2 className='text-center' style={{ fontSize: '3rem' }}>Uniforms</h2>
 
-        <p >
-        We will be sticking with the uniform tradition for our in-person days. This year, students will be required to wear formal and daily uniform items. Logo items will be available to order throughout the year. Please see the uniform policy below for more specific information and where to order logo items. Click <a href='http://www.schoolbelles.com/myschool/M37/s2950' target='_blank'>HERE</a> to be relocated to our School Belles uniform page. Our school code is S2950. There are several required items from School Belles.
-        </p>
-        <img style={{maxWidth: '100%', marginBottom: '1rem'}} src={uniforms} className='col-8' alt='document explaining Kardia uniform policy' />
-        <p>These are the uniform requirements for the 2023-2024 school year. Please note that there are several changes.</p>
-    </div>
-</div>  )
+                <p >
+                    We will be sticking with the uniform tradition for our in-person days. This year, students will be required to wear formal and daily uniform items. Logo items will be available to order throughout the year. Please see the uniform policy below for more specific information and where to order logo items. Click <a href='http://www.schoolbelles.com/myschool/M37/s2950' target='_blank'>HERE</a> to be relocated to our School Belles uniform page. Our school code is S2950. There are several required items from School Belles.
+                </p>
+                <img style={{ maxWidth: '100%', marginBottom: '1rem' }} src={uniforms} className='col-xl-5 mx-xl-2' alt='document explaining Kardia uniform policy' />
+                <img style={{ maxWidth: '100%', marginBottom: '1rem' }} src={prekuniforms} className='col-xl-5 mx-xl-2' alt='document explaining Kardia uniform policy' />
+                <p>These are the uniform requirements for the 2023-2024 school year. Please note that there are several changes.</p>
+            </div>
+        </div>)
 }
 
 export default Uniforms
