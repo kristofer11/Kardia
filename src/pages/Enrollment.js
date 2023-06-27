f650bb9e57fb50153f6151381f141f8193ea9902
Kris Hvattum <klhvattum@live.com>
2023-06-27 15:51:46 -0700

    Style images

--- src/pages/Enrollment.js
+++ src/pages/Enrollment.js
@@ -33,18 +33,18 @@ const Enrollment = () => {
                     <h3 className='text-center pt-4'>We are a new family and would like to apply</h3>
                     <p>We are glad you are interested in joining our community! Please click <a href='https://sycamore.school/login?schoolId=4125' target='_blank'>HERE</a> to access the new family application (see below for instructions on how to navigate the admissions platform). This link will redirect you to our online application portal. Once you have completed the online application and submitted all required signed forms, you will be called by the Head of School to chat about the next steps. To fully save your place, you will need to mail in enrollment fees to our address listed on the contact page. Enrollment for new families will open on Wednesday, February 15, 2023.</p>
                     <img src={enroll1} className='col-12' />
-                    <p>
+                    <p className='pt-3'>
                         <em>Click on the register button, and follow the directions given.
                         </em>
                     </p>
                     <img src={enroll2} className='col-12' />
-                    <p>
+                    <p className='pt-3'>
                         <em>As a new family, click on the “I need a new account” register button.
 
                         </em>
                     </p>
                     <img src={enroll3} className='col-12' />
-                    <p>
+                    <p className='pt-3'>
                         <em>Fill out the necessary information to create an account. This will put you in our system and make it so you can save progress and come back to it later if needed. You will not be required to get it all done in one sitting.
                         </em>
                     </p>
