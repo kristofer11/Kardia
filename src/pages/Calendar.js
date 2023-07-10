bde5a8fa8f21c62ea1c50ea373bdfa4714df292b
Kris Hvattum <klhvattum@live.com>
2023-07-10 14:16:34 -0700

    remove old calendar

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -10,15 +10,10 @@ const Calendar = () => {
         <h2 className='text-center' style={{fontSize: '3rem'}}>Calendar</h2>
         
             <p >
-            Here is the calendar for the 2022-2023 School Year! Don’t forget, we are a small school, so dates may be subject to change. However, We will give plenty of notice if anything does change. Whenever there are changes to the calendar, it will be updated here as well.<br /><strong>Click on a calendar below to download.</strong><br/><em>Please note that the 2023-2024 calendar is tentative at this point.</em>
+            Here is the proposed calendar for the 2023-2024 School Year! Don’t forget, we are a small school, so dates may be subject to change. However, We will give plenty of notice if anything does change. Whenever there are changes to the calendar, it will be updated here as well.<br /><strong>Click on the calendar below to download.</strong><br/><em>Please note that the 2023-2024 calendar is tentative at this point.</em>
             </p>  
         <div className='calendar-div'>
-        
     
-        <div>    
-            <p>2022-2023</p>
-            <a href={Calendar22} download='2022-23 Kardia Calendar.pdf'><img src={calendar} className='col-10 mb-5 calendar' alt='2022-23 calendar' /> </a>           
-        </div>
         <div>
             <p>2023-2024 (proposed)</p>
             <a href={Calendar23} download='2023-24 Kardia Calendar.pdf'><img src={proposedCalendar} className='col-10 mb-5 calendar' alt='Proposed 2023-24 calendar2' /></a>
