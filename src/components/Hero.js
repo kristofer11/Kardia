7c5e366ca87161f381598f3c3b53d75331c43af7
Kris Hvattum <klhvattum@live.com>
2023-08-15 17:09:44 -0700

    add animation to hero text

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,18 +1,37 @@
 import { Parallax } from "react-parallax";
+import { useState, useEffect } from 'react'
+import { useSpring, animated } from 'react-spring'
 
 const Hero = ({ img, alt, title, strength }) => {
+    const [toggle, setToggle] = useState(false);
+
+    const animatedStyle = useSpring({
+        opacity: toggle ? 1 : 0,
+        transform: toggle ? 'scale(1,1)' : 'scale(0.7,0.7)',
+        config: { duration: 400 }
+      });
+
+      useEffect(() => {
+        setToggle(true);
+      }, [])
+
     return (
-        <Parallax
-            bgImage={img}
-            strength={strength}
-            bgImageAlt={alt}
-        >
-            <div className='hero'>
-                <div className='hero-text'>
-                    {title}         
+
+            <Parallax
+                bgImage={img}
+                strength={strength}
+                bgImageAlt={alt}
+            >
+        <animated.div style={animatedStyle}>                
+                <div className='hero'>
+                    <div className='hero-text'>
+                        {title}         
+                    </div>
                 </div>
-            </div>
-        </Parallax>
+        </animated.div>                
+            </Parallax>            
+
+
     )
 
 }
